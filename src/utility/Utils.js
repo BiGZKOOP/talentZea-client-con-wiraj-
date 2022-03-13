04750248fcd79adb45e5211203993154d794fd01
Udara J(SiniSTeR) <67888873+udaraJ08@users.noreply.github.com>
2022-03-13 10:02:03 +0530

    slight changes

--- src/utility/Utils.js
+++ src/utility/Utils.js
@@ -1,5 +1,4 @@
 // ** Checks if an object is empty (returns boolean)
-import welcome from "../assets/audio/welcome.mp3"
 
 export const isObjEmpty = obj => Object.keys(obj).length === 0
 
@@ -89,9 +88,4 @@ export const scrollToTopUTIL = () => {
     setTimeout(() => {
         window.scrollTo({top: 0, behavior: 'smooth'})
     }, 0)
-}
-
-export const playAudio = async () => {
-    const audio = new Audio(welcome)
-    await audio.play()
 }
