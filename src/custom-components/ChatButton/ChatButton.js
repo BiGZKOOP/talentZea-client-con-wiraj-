16b6ad9eb98f52f4a83979fd090165481292dd2b
Udara J(SiniSTeR) <67888873+udaraJ08@users.noreply.github.com>
2022-05-25 18:39:11 +0530

    handle chat btn

---
+++ src/custom-components/ChatButton/ChatButton.js
@@ -0,0 +1,25 @@
+import {Card} from "reactstrap"
+import {MessageCircle, MessageSquare, Music, VolumeX} from "react-feather"
+import {fireAlertCustom} from "../../utility/custom-util"
+import {Auth} from "aws-amplify"
+import {useEffect, useState} from "react"
+
+
+const ChatButton = () => {
+    
+    const [logged, setLogged] = useState(false)
+
+    useEffect(async () => {
+        if (await Auth.currentAuthenticatedUser()) setLogged(true)
+    }, [])
+
+    const alertFire = () => {
+        fireAlertCustom("Oops !", "Please sign in to chat with us", "warning")
+    }
+
+    return <Card hidden={logged} onClick={alertFire} className="chat-btn d-center clickable">
+        <MessageSquare size={30} className="text-light font-bold"/>
+    </Card>
+}
+
+export default ChatButton
