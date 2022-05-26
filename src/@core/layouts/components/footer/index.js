02d36979b7856a28157dcc1b7cd7b326c29bed2d
Udara J(SiniSTeR) <67888873+udaraJ08@users.noreply.github.com>
2022-05-26 08:43:38 +0530

    social btn redirections added

--- src/@core/layouts/components/footer/index.js
+++ src/@core/layouts/components/footer/index.js
@@ -1,10 +1,14 @@
 // ** Icons Import
 import {Facebook, Heart, Instagram, Mail, Phone} from 'react-feather'
 import {Card} from "reactstrap"
+import {useHistory} from "react-router-dom"
 
 const Footer = () => {
 
+    const history = useHistory()
+
     const navToSocial = (url) => {
+        console.log(url)
         window.location.href = url
     }
 
@@ -13,11 +17,17 @@ const Footer = () => {
             <p className="f-Staatliches">Connect with us</p>
             <div className="d-flex mb-1">
                 <Facebook
-                    onClick={() => navToSocial("https://www.facebook.com/")}
+                    onClick={() => navToSocial("https://www.facebook.com/TalentZea")}
                     className="mr-1 scalable cursor-pointer"/>
-                <Instagram className="mr-1 scalable cursor-pointer"/>
-                <Mail className="mr-1 scalable cursor-pointer"/>
-                <Phone className="mr-1 scalable cursor-pointer"/>
+                <Instagram
+                    onClick={() => navToSocial("https://www.instagram.com/talent_zea/")}
+                    className="mr-1 scalable cursor-pointer"/>
+                <Mail
+                    onClick={() => history.push("/contact")}
+                    className="mr-1 scalable cursor-pointer"/>
+                <Phone
+                    onClick={() => history.push("/contact")}
+                    className="mr-1 scalable cursor-pointer"/>
             </div>
             <p className='clearfix mb-0 word-break-all'>
       <span className='float-md-start d-block d-md-inline-block f-Londrina text-medium'>
