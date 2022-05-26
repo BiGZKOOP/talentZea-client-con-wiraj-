dc55fcbc88b00124258917d1cdf69dfa5aee02af
Udara J(SiniSTeR) <67888873+udaraJ08@users.noreply.github.com>
2022-05-26 08:38:50 +0530

    fwp bug resolved

--- src/@core/layouts/components/footer/index.js
+++ src/@core/layouts/components/footer/index.js
@@ -3,14 +3,21 @@ import {Facebook, Heart, Instagram, Mail, Phone} from 'react-feather'
 import {Card} from "reactstrap"
 
 const Footer = () => {
+
+    const navToSocial = (url) => {
+        window.location.href = url
+    }
+
     return (
         <Card className="m-0 main-service-back container-fluid text-light p-1 d-center bg-semi-dark">
             <p className="f-Staatliches">Connect with us</p>
             <div className="d-flex mb-1">
-                <Facebook className="mr-1"/>
-                <Instagram className="mr-1"/>
-                <Mail className="mr-1"/>
-                <Phone className="mr-1"/>
+                <Facebook
+                    onClick={() => navToSocial("https://www.facebook.com/")}
+                    className="mr-1 scalable cursor-pointer"/>
+                <Instagram className="mr-1 scalable cursor-pointer"/>
+                <Mail className="mr-1 scalable cursor-pointer"/>
+                <Phone className="mr-1 scalable cursor-pointer"/>
             </div>
             <p className='clearfix mb-0 word-break-all'>
       <span className='float-md-start d-block d-md-inline-block f-Londrina text-medium'>
